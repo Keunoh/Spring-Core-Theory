46bdc88ebe5bfc338d2bf1a0c1c940c787c79a82
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-02 17:48:15 +0900

    init: gradlew bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,6 @@
 @rem
 @rem Copyright 2015 the original author or authors.
-@rem bat?
+@rem batman
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
