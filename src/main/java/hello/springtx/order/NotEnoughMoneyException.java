1486dd0bc9e3892cec5eefa5f210e15040e12d60
kaltz <rmsdhcjswo1@gmail.com>
2023-12-19 14:21:47 +0900

    Checked && UnChecked Exception case to rollback or not

--- src/main/java/hello/springtx/order/NotEnoughMoneyException.java
+++ src/main/java/hello/springtx/order/NotEnoughMoneyException.java
@@ -1,2 +1,9 @@
-package hello.springtx.order;public class NotEnoughMoneyException {
+package hello.springtx.order;
+
+public class NotEnoughMoneyException extends Exception {
+    //Exception 상속 받았으므로 체크예외가 된다.
+
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
 }
